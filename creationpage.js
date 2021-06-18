3915adc3490ff4d37a801968fe6c938eb687972d
krakenoide <guillaume.bortoli@gmail.com>
2021-06-18 10:34:51 +0200

    moved imports on creation page

--- creationpage.js
+++ creationpage.js
@@ -1,10 +1,11 @@
-function verscreationpage() {
+
 
 import {Libs} from './Libs.js';
 import {Message} from './Message.js';
 import {User} from './User.js';
 import {Topic} from './Topic.js';
 
+function verscreationpage() {
 
 let display=document.getElementById("display");
 
